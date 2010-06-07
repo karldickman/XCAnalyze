b513fc2aa81431e34a464b195d1f80d8a280eccf
Karl Dickman <karldickman@gmail.com>
2010-06-07 14:07:34 -0700

    Score formatter should not give places to schools that scored no points.

--- Hytek/ScoreFormatter.cs
+++ Hytek/ScoreFormatter.cs
@@ -33,26 +33,43 @@ namespace XCAnalyze.Hytek
             Performance runner;
             int? score;
             List<object[]> values = new List<object[]> ();
-            for (int i = 0; i < scores.Count; i++) {
+            for (int i = 0; i < scores.Count; i++)
+            {
                 int n = 0;
                 object[] valueRow = new object[Header.Length];
-                valueRow[n++] = (i + 1).ToString ();
+                if (scores[i].Score () != null)
+                {
+                    valueRow[n++] = (i + 1).ToString ();
+                }
+                else
+                {
+                    valueRow[n++] = "";
+                }
                 valueRow[n++] = scores[i].School.Name;
                 score = scores[i].Score ();
-                if (score != null) {
+                if (score != null)
+                {
                     valueRow[n++] = score.Value.ToString ();
-                } else {
+                }
+                else
+                {
                     valueRow[n++] = null;
                 }
                 for (int j = 0; j < 7; j++) {
-                    if (j < scores[i].Runners.Count) {
+                    if (j < scores[i].Runners.Count)
+                    {
                         runner = scores[i].Runners[j];
-                        if (runner.Points != null) {
+                        if (runner.Points != null)
+                        {
                             valueRow[n++] = runner.Points.ToString ();
-                        } else {
+                        }
+                        else
+                        {
                             valueRow[n++] = null;
                         }
-                    } else {
+                    }
+                    else
+                    {
                         valueRow[n++] = null;
                     }
                 }
@@ -60,7 +77,8 @@ namespace XCAnalyze.Hytek
                 valueRow = new object[Header.Length];
                 valueRow[1] = "  Top 5 Avg: " + scores[i].TopFiveAverage ().Time;
                 values.Add (valueRow);
-                if (scores[i].Runners.Count > 5) {
+                if (scores[i].Runners.Count > 5)
+                {
                     valueRow = new object[Header.Length];
                     valueRow[1] = "  Top 7 Avg: " + scores[i].TopSevenAverage ().Time;
                     values.Add (valueRow);
