c865e0d175860131f3f5ef76b37ae38a9d543702
Karl Dickman <karldickman@gmail.com>
2010-06-10 04:32:40 -0700

    The gender enumeration is not necesary.  Just use the class.

--- Model/Gender.cs
+++ Model/Gender.cs
@@ -3,31 +3,21 @@ using System;
 namespace XCAnalyze.Model
 {
 	/// <summary>
-	/// A handy enumeration for gender.
-	/// </summary>
-	internal enum gender { M, F }
-
-	/// <summary>
 	/// This class can have only two instances, the male instance and the female
     /// instance.  This is an example of the doubleton pattern.
     /// (http://thedailywtf.com/articles/the-doubleton-patten.aspx)
 	/// </summary>
 	public class Gender : IComparable<Gender>
-	{
-        /// <summary>
-        /// The gender of the instance.
-        /// </summary>
-		private gender _gender;
-        
+	{        
         /// <summary>
         /// The male instance
         /// </summary>
-		public static readonly Gender MALE = new Gender (gender.M);
+		public static readonly Gender MALE = new Gender ();
         
         /// <summary>
         /// The female instance.
         /// </summary>
-		public static readonly Gender FEMALE = new Gender (gender.F);
+		public static readonly Gender FEMALE = new Gender ();
           
         /// <summary>
         /// Is this the male instance?
@@ -38,11 +28,6 @@ namespace XCAnalyze.Model
         /// Is this the female instance.
         /// </summary>
         public bool IsFemale { get { return this == FEMALE; } }
-        
-		internal Gender (gender gender)
-		{
-			this._gender = gender;
-		}
 
 		/// <summary>
 		/// Get a gender instance from a string.  "M" returns the male instance, "F" the female instance.
@@ -74,11 +59,6 @@ namespace XCAnalyze.Model
 				return -1;
 			}
 			return 1;
-		}
-
-		override public int GetHashCode ()
-		{
-			return _gender.GetHashCode ();
 		}
 
 		/// <summary>
